8504bb4461ea09294a97d218bd91d352544d393c
vla5924 <vla5924@gmail.com>
2021-02-18 20:50:29 +0300

    logical regression: classifier

--- algorithms/include/algorithms/logistical_regression.hpp
+++ algorithms/include/algorithms/logistical_regression.hpp
@@ -3,19 +3,41 @@
 namespace Algorithms
 {
 
+/**
+ * Logistical regression classifier
+ * 
+ * @todo Description
+ */
 class LogisticalRegression
 {
 public:
+    /**
+     * Single dataset entry
+     * 
+     * std::vector<double> Vector of feature values
+     * bool                Target value (does feature belong to class or not)
+     */
     using Pair = std::pair<std::vector<double>, bool>;
 
 protected:
-    std::vector<Pair> m_training;
+    // Count of features in dataset
+    size_t m_feature_count;
+
+    // Dataset for model to be pre-trained
+    std::vector<Pair> m_dataset;
+
+    // Coefficients calculated with dataset given for pre-training
+    std::vector<double> m_theta;
+
+    static double sigmoid(double x);
+
+    void train();
 
 public:
-    LogisticalRegression(const std::vector<Pair>& training);
+    LogisticalRegression(const std::vector<Pair>& dataset);
     ~LogisticalRegression() = default;
 
-    std::vector<double> classify(const std::vector<Pair>& inputs);
+    double classify(const std::vector<double>& inputs);
 };
 
 }
--- algorithms/src/logistical_regression.cpp
+++ algorithms/src/logistical_regression.cpp
@@ -1,14 +1,42 @@
 #include "logistical_regression.hpp"
 
+#include <cmath>
+#include <stdexcept>
+
 using namespace Algorithms;
 
+double LogisticalRegression::sigmoid(double x)
+{
+    return 1. / (1. + std::exp(-x));
+}
 
-LogisticalRegression::LogisticalRegression(const std::vector<Pair>& training)
+void LogisticalRegression::train()
 {
 
 }
 
-std::vector<double> LogisticalRegression::classify(const std::vector<Pair>& inputs)
+LogisticalRegression::LogisticalRegression(const std::vector<Pair>& dataset)
 {
-    return std::vector<double>();
+    if (dataset.empty())
+        throw std::runtime_error("Empty dataset is not allowed");
+
+    m_feature_count = dataset.front().first.size();
+    for (const Pair& entry : dataset)
+        if (entry.first.size() != m_feature_count)
+            throw std::runtime_error("Dataset entries have different sizes");
+
+    m_dataset = dataset;
+    train();
+}
+
+double LogisticalRegression::classify(const std::vector<double>& inputs)
+{
+    if (inputs.size() != m_feature_count)
+        throw std::runtime_error("Input does not have the same size as dataset");
+
+    double prod = 0;
+    for (size_t i = 0; i < m_feature_count; i++)
+        prod += m_theta[i] * inputs[i];
+
+    return sigmoid(prod);
 }
